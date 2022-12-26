53b7c4f84a4dcc2d67befa25d157dde894b3bb26
makhosazana-aghoghovwia <mp.aghoghovwia@outlook.com>
2022-12-25 21:49:22 -0700

    Resized icon images and shifted margins

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function displayForecast(response) {
   <div class="weather-forecast-date">${formatForecastDay(forecastDay.dt)}</div>
   <img src="http://openweathermap.org/img/wn/${
     forecastDay.weather[0].icon
-  }@2x.png" alt="" width="42" />
+  }@2x.png" alt="" width="50" />
 
   <div class="forecast-temperature">
                   <span class="forecast-temperature-high"> ${Math.round(
