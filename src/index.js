393720026d13944a9c202b505880dec970b58b92
makhosazana-aghoghovwia <mp.aghoghovwia@outlook.com>
2022-12-25 18:41:00 -0700

    Updated JS to display forecast and centered weather-forecast day names

--- src/index.js
+++ src/index.js
@@ -23,29 +23,50 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-  <div class="weather-forecast-date">${day}</div>
-  <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="42" />
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+  <div class="weather-forecast-date">${formatForecastDay(forecastDay.dt)}</div>
+  <img src="http://openweathermap.org/img/wn/${
+    forecastDay.weather[0].icon
+  }@2x.png" alt="" width="42" />
 
   <div class="forecast-temperature">
-                  <span class="forecast-temperature-high">12° </span>
-                  <span class="forecast-temperature-low">1°</span>
+                  <span class="forecast-temperature-high"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="forecast-temperature-low">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
                 </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e6c2364656962bdcb16bc352fc42569a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -74,6 +95,8 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function convertToFahrenheit(event) {
@@ -114,4 +137,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 searchCity("Edmonton");
-displayForecast();
